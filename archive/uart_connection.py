831b71097574c4d11ac3b3c386996e569ccbefd7
Simon Völkl <svoelkl@witron.de>
2026-04-19 23:22:45 +0200

    properly archived files

--- archive/uart_connection.py
+++ archive/uart_connection.py
@@ -1,43 +1,43 @@
-# Aufbau: Pico 
-# - UART0-TX (1) mit UART1-RX (12)
-# - UART0-RX (2) mit UART1-TX (11)
+# # Aufbau: Pico 
+# # - UART0-TX (1) mit UART1-RX (12)
+# # - UART0-RX (2) mit UART1-TX (11)
 
-# Baudrate: typischerweise 9600
-# Anzahl der Datenbits: typischerweise 8
-# Anzahl der Stopbits: Typischerweise 1
-# Parität: Typischerweise None oder 0
+# # Baudrate: typischerweise 9600
+# # Anzahl der Datenbits: typischerweise 8
+# # Anzahl der Stopbits: Typischerweise 1
+# # Parität: Typischerweise None oder 0
 
-# Bibliotheken laden
-from machine import UART, Pin
-from time import sleep
+# # Bibliotheken laden
+# from machine import UART, Pin
+# from time import sleep
 
-# adc
-adc_controller = machine.ADC(26)
+# # adc
+# adc_controller = machine.ADC(26)
 
-# UART0 initialisieren
-uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
-print('UART0:', uart0)
-print()
+# # UART0 initialisieren
+# uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
+# print('UART0:', uart0)
+# print()
 
-# UART1 initialisieren
-uart1 = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9), bits=8, parity=None, stop=1)
-print('UART1:', uart1)
-print()
+# # UART1 initialisieren
+# uart1 = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9), bits=8, parity=None, stop=1)
+# print('UART1:', uart1)
+# print()
 
-# Hauptprogramm
-while True:
-    gas_wert = adc_controller.read_u16()
+# # Hauptprogramm
+# while True:
+#     gas_wert = adc_controller.read_u16()
     
-    # Daten senden
-    print('Gaswert:', gas_wert)
-    uart0.write(str(gas_wert).encode('utf-8'))
-    sleep(0.001)
+#     # Daten senden
+#     print('Gaswert:', gas_wert)
+#     uart0.write(str(gas_wert).encode('utf-8'))
+#     sleep(0.001)
     
-    # Daten empfangen und ausgeben
-    rxData = uart1.readline()
-    try:
-        print('Daten empfangen:', rxData.decode('utf-8'))
-    except (AttributeError):
-        print('Verbindung prüfen')
-    sleep(0.001)
-    print()
+#     # Daten empfangen und ausgeben
+#     rxData = uart1.readline()
+#     try:
+#         print('Daten empfangen:', rxData.decode('utf-8'))
+#     except (AttributeError):
+#         print('Verbindung prüfen')
+#     sleep(0.001)
+#     print()
